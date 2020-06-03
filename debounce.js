6c60ce8336c25ee7a394fe3e2f4861bc2eb54c50
VAIBHAV PRASAD <prasadvaibhav24@gmail.com>
2020-06-04 00:10:25 +0530

    Create debounce.js

---
+++ debounce.js
@@ -0,0 +1,22 @@
+// The following function takes a function and a timeout as input and returns a function which if executed before the timeout
+// happens, resets the timer.
+
+
+function debounce (timeout, fn) {
+    let intervalhandler = null;
+    return function (...b) {
+        clearTimeout(intervalhandler);
+        intervalhandler = setTimeout(() => {
+            return fn.apply(this, b);
+        }, timeout);
+    }
+}
+
+function addNos (a,b) {
+    console.log(a + b);
+}
+// debounce(5000, addNos)(4, 5);
+var debouncedAdd = debounce(5000, addNos);
+
+debouncedAdd(4,5);
+
